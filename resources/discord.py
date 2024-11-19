50e51fdfc4f5df84ce9c06975817ec51591ff079
Chris Riccomini <criccomini@apache.org>
2024-11-18 17:51:21 -0800

    SlateDB was not a snowflake (discord-speak for integer ID)

--- resources/discord.py
+++ resources/discord.py
@@ -4,7 +4,8 @@ import pulumi_discord
 def assign_committer_role(user: str):
     pulumi_discord.MemberRoles(
         f"assign_discord_committer_role_{user}",
-        server_id="SlateDB",
+        # 1232385660460204122 is the server ID for the "SlateDB" server
+        server_id="1232385660460204122",
         user_id=user,
         roles=[
             # 1303423411908903005 is the role ID for the "committer" role
