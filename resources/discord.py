2b0a145145c50fc98b8e6b7e3dc5d5dbdab8d0ca
Chris Riccomini <criccomini@apache.org>
2024-11-18 17:45:36 -0800

    Use SlateDB as Discord server ID

--- resources/discord.py
+++ resources/discord.py
@@ -4,8 +4,7 @@ import pulumi_discord
 def assign_committer_role(user: str):
     pulumi_discord.MemberRoles(
         f"assign_discord_committer_role_{user}",
-        # 1232385660460204122 is the server ID for the "SlateDB" server
-        server_id="1232385660460204122",
+        server_id="SlateDB",
         user_id=user,
         roles=[
             # 1303423411908903005 is the role ID for the "committer" role
