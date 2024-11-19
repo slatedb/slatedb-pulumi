e90540cc0fe592310daa8d8b6c59df02bcc5b518
Chris Riccomini <criccomini@apache.org>
2024-11-18 16:58:07 -0800

    Make crate calls idempotent

--- resources/crates.py
+++ resources/crates.py
@@ -2,8 +2,18 @@ from pulumi_command import local
 
 
 def add_owner(username: str, crate: str):
-        add_owner_command = local.Command(
+    local.Command(
         f"add_crate_owner_{username}_{crate}",
-        create=f"cargo owner --add {username} {crate}",
-        delete=f"cargo owner --remove {username} {crate}",
+        create=(
+            f"if ! cargo owner --list {crate} | grep -q {username}; then "
+            f"cargo owner --add {username} {crate}; "
+            f"else echo 'User {username} is already an owner of {crate}'; "
+            f"fi"
+        ),
+        delete=(
+            f"if cargo owner --list {crate} | grep -q {username}; then "
+            f"cargo owner --remove {username} {crate}; "
+            f"else echo 'User {username} is not an owner of {crate}'; "
+            f"fi"
+        ),
     )
